6363d873c82d15243c6041dbe6db34b038f5c4ec
Toni Alatalo <toni@playsign.net>
2019-08-20 12:31:03 +0300

    wrap parsing in a couple functions to build for providing data for visualization

--- pua.py
+++ pua.py
@@ -1,48 +1,53 @@
 import json
 
-with open("data/Zotac06/282c0f304a7d160ff93664ad17e0a5db-2018.04.17-14.54.24.analytics", "r") as read_file:
-    data = json.load(read_file)
-
-#print(data)
-
 #string constants to avoid typos in parsing .. todo?
 
-sessionId = data['sessionId']
+def parse_events(events):
+    countTeleport = 0
+    countInteract = 0
+    for e in events:
+        ename = e['eventName']
+        #print(ename)
+        if ename == 'Teleport':
+            countTeleport += 1
+        if ename == 'Interact':
+            countInteract += 1
+        
+        #here we can get Teleport coord data within level, until hit another BeginLevel
+        if ename == 'BeginLevel':
+            print("new BeginLevel")
+            print("Teleport count in prev was: %d" % countTeleport)
+            print("Interact count in prev was: %d" % countInteract)
+            countTeleport = 0
+            countInteract = 0
 
-events = data['events']
+            leveldata = {i['name']:i['value'] for i in e['attributes']}
+            print(leveldata)
 
-start = events[0]
-#print(start)
-assert start['eventName'] == 'StartSession'
+def get_session():
+    with open("data/Zotac06/282c0f304a7d160ff93664ad17e0a5db-2018.04.17-14.54.24.analytics", "r") as read_file:
+        data = json.load(read_file)
 
-beginLevel = events[1]
-#print(beginLevel)
-assert beginLevel['eventName'] == 'BeginLevel'
-leveldata = {i['name']:i['value'] for i in beginLevel['attributes']}
-#for attr in levelattrs:
-print(leveldata)
-
-#first level should always be the library lobby
-assert leveldata['LevelName'] == 'MainLibrary'
-
-countTeleport = 0
-countInteract = 0
-for e in events[2:]:
-    ename = e['eventName']
-    #print(ename)
-    if ename == 'Teleport':
-        countTeleport += 1
-    if ename == 'Interact':
-        countInteract += 1
+    #print(data)
     
-    #here we can get Teleport coord data within level, until hit another BeginLevel
-    if ename == 'BeginLevel':
-        print("new BeginLevel")
-        print("Teleport count in prev was: %d" % countTeleport)
-        print("Interact count in prev was: %d" % countInteract)
-        countTeleport = 0
-        countInteract = 0
+    sessionId = data['sessionId']
+    events = data['events']
 
-        leveldata = {i['name']:i['value'] for i in e['attributes']}
-        print(leveldata)
+    start = events[0]
+    #print(start)
+    assert start['eventName'] == 'StartSession'
 
+    beginLevel = events[1]
+    #print(beginLevel)
+    assert beginLevel['eventName'] == 'BeginLevel'
+    leveldata = {i['name']:i['value'] for i in beginLevel['attributes']}
+    #for attr in levelattrs:
+    print(leveldata)
+
+    #first level should always be the library lobby
+    assert leveldata['LevelName'] == 'MainLibrary'
+
+    parse_events(events[2:])
+
+if __name__ == '__main__':
+    get_session()
