291ddd7d14078146e362bc112f61c5c24603691e
Toni Alatalo <toni@playsign.net>
2019-08-20 14:44:46 +0300

    create a svg per level visit. use min&max to set viewport to show all. retain original coordinates so they continue to match with unreal scene coords

--- svgmap.py
+++ svgmap.py
@@ -1,15 +1,28 @@
 import pua
 import svgwrite
 
-dwg = svgwrite.Drawing('test.svg', profile='tiny')
+def draw(x, y):
+    #print(location)
+    dwg.add(dwg.circle((x, y), 10)) #stroke=svgwrite.rgb(10, 10, 16, '%')))
 
-def draw(location):
-    print(location)
+session = pua.get_session() #could give the source log file pathname
 
+"""now a picture per level visit. does not check names of levels or anything yet."""
+for i, level in enumerate(session): #.levels: #could include metadata, now is just the list of levels
+    dwg = svgwrite.Drawing(f'level_{i+1}.svg', profile='tiny')
+    all_x = []
+    all_y = []
 
-session = pua.get_session()
-
-for level in session: #.levels: #could include metadata, now is just the list of levels
     for t in level: #.teleports:
-        draw(t) #.location) #what if we wanna show what happened between the teleports?
+        x, y, z = t #.location) 
+        draw(x, y)  #what if we wanna show what happened between the teleports? (interacts)
+        all_x.append(x)
+        all_y.append(y)
 
+    min_x = min(all_x)
+    max_x = max(all_x)
+    min_y = min(all_y)
+    max_y = max(all_y)
+
+    dwg.viewbox(min_x, min_y, max_x - min_x, max_y - min_y)
+    dwg.save()
